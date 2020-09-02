817047591c4beb88cf58e74877525f5aaf8fd5b6
Anderson Granja <andersin100por100to@gmail.com>
2020-09-02 19:34:35 -0300

    Crição da nova página interna de rádio

---
+++ inc/noticias/aside-radio-interna.inc.php
@@ -0,0 +1,35 @@
+<aside id="aside-noticias" class="nao-imprimir">
+    <div class="lateral__mais-lidas">
+        <h2 class="sr-only">Relacionadas</h2>
+        <div class="lateral__mais-lidas-header">
+            <h3 class="lateral__mais-lidas-title">Notícias relacionadas</h3>
+        </div>
+        <?php include __DIR__ . '/relacionadas.inc.php';?>
+    </div>
+
+    <div class="lateral__mais-lidas">
+        <h2 class="sr-only">Últimas notícias postadas</h2>
+        <div class="lateral__mais-lidas-header">
+            <h3 class="lateral__mais-lidas-title">Últimas</h3>
+        </div>
+        <?php include __DIR__ . '/recentes.inc.php';?>
+    </div>
+
+    <div class="lateral__mais-lidas">
+        <h2 class="sr-only">Temas</h2>
+        <div class="lateral__mais-lidas-header">
+            <h3 class="lateral__mais-lidas-title">Navegue por temas</h3>
+        </div>
+            <?php include __DIR__ . '/temas.inc.php';?>
+    </div>
+
+    <div class="lateral__mais-lidas">
+        <h2 class="sr-only">Relacionadas</h2>
+        <div class="lateral__mais-lidas-header">
+            <h3 class="lateral__mais-lidas-title">Quem leu isso, leu também</h3>
+        </div>
+        <?php include __DIR__ . '/relacionadas.inc.php';?>
+    </div>
+
+
+</aside>
---
+++ radio-interna.php
@@ -0,0 +1,137 @@
+<?php
+include __DIR__ . '/inc/config.inc.php';
+$asideinterna   = false;
+$asidenoticia   = false;
+$titulo         = 'Lei dos Partidos Políticos completa 24 anos nesta quinta (19)';
+$compartilhar   = false;
+$tags           = false;
+$gestor         = false;
+$ocultartitulo  = true;
+$maisvisitados  = false;
+$relacionadas   = false;
+include __DIR__ . '/inc/header.inc.php';
+?>
+<div class="order-elementos-header">
+    <header class="noticia-cabecalho">
+        <h2 class="noticia-titulo">Lei dos Partidos Políticos completa 24 anos nesta quinta (19)</h2>
+        <p class="noticia-subtitulo">Ao longo do tempo, embora tenha passado por reformas, a norma manteve seu papel de
+            assegurar a representatividade e a autonomia das agremiações </p>
+    </header>
+    <div class="noticia__conteudo">
+        <div class="noticia__linha">
+            <div class="noticia-dados">
+                <div class="autor">Por <span class="nome-autor">Sebastian Abreu</span> e <span class="nome-autor">Túlio
+                        Maravilha</span> / Revisão: <span class="nome-revisor">Keisuke Honda</span> - <span
+                        class="local">TSE</span></div>
+                <div class="data-hora">17/01/2020 13:30 - <span class="atualizacao">Atualizado em 19/01/2020 -
+                        18:30</span>
+                </div>
+            </div>
+            <?php include __DIR__ . '/inc/share.inc.php';?>
+        </div>
+    </div>
+    <?php include __DIR__ . '/inc/imagem-topo.inc.php';?>
+</div>
+<?php include __DIR__ . '/inc/noticias/sumario.inc.php';?>
+<div class="noticia__conteudo">
+    <div class="noticia__linha">
+        <section id="texto-conteudo" class="texto-noticias-internas">
+            <div class="tab-content sem-galeria">
+                <div role="tabpanel" class="tab-pane active" id="ancora-1">
+                    <p>A Lei dos Partidos Políticos (Lei nº 9.096/1995) completa 24 anos nesta quinta-feira (19). Ao
+                        longo desse tempo, embora tenha passado por diversas reformas normativas, a lei manteve seu
+                        papel fundamental de assegurar a representatividade e a autonomia das agremiações, prerrogativas
+                        garantidas pela Constituição Federal de 1988.</p>
+                    <p>Sancionada em 19 de setembro de 1995, a legislação substituiu a Lei Orgânica dos Partidos (Lei n°
+                        5.682/1971), remanescente ainda do período do regime militar no Brasil. Assim, as agremiações
+                        passaram a ter autonomia, e a natureza jurídica das legendas passou a ser privada, entre outras
+                        alterações.</p>
+                    <p>O artigo 17 da Constituição Federal estabelece como livre a criação, a fusão, a incorporação e a
+                        extinção de partidos políticos, resguardados a soberania nacional, o regime democrático, o
+                        pluripartidarismo e os direitos fundamentais da pessoa humana. O texto constitucional exige que
+                        os partidos tenham caráter nacional, prestem contas à Justiça Eleitoral e tenham funcionamento
+                        parlamentar de acordo com a lei vigente.</p>
+                    <p>De acordo com o secretário judiciário do TSE, Fernando Alencastro, a Lei nº 9.096 assegura às
+                        legendas autonomia para definir sua estrutura interna, organização e funcionamento, bem como
+                        para adotar os critérios de escolha e o regime de suas coligações eleitorais, sem a
+                        obrigatoriedade de vinculação entre as candidaturas em âmbito nacional, estadual, distrital ou
+                        municipal.</p>
+                    <p>Alencastro explica que a Lei dos Partidos Políticos também regulamenta outros importantes pontos,
+                        tais como: uso e distribuição do Fundo Partidário; filiação e fidelidade partidária; prestação
+                        de contas; e acesso gratuito ao rádio e à TV. “Em 2009, ocorreu uma mudança significativa no
+                        artigo 37 da Lei, fazendo com que a prestação de contas das agremiações à Justiça Eleitoral
+                        passasse de procedimento administrativo para jurisdicional. Isso fez com que se ampliasse a
+                        possibilidade de defesa, com a participação de advogados na prestação de contas”, revelou.</p>
+                    <p>Confira, a seguir, alguns dos principais pontos e mudanças ocorridas ao longo da vigência da
+                        norma:</p>
+                    <h3 id="titulo-1">Criação e registro dos partidos</h3>
+                    <p>A Lei 9.096/1995 estabelece regras gerais para criação e registro dos partidos. A Resolução do
+                        TSE nº 23.282/2010, por sua vez, detalha o que é necessário para se criar uma nova legenda. Quem
+                        defere ou não a criação do partido é o TSE. Até a protocolização do pedido neste Tribunal, o
+                        partido em formação deve cumprir exigências, tais como: a elaboração do programa e do estatuto
+                        por pelo menos 101 fundadores; o apoio de eleitores correspondente a, no mínimo, 0,5% dos votos
+                        dados na última eleição geral para a Câmara dos Deputados, distribuído por um terço ou mais dos
+                        Estados; e a certidão de apoio dos eleitores, emitida por cartórios eleitorais.</p>
+                    <h3 id="titulo-2">Fundo Partidário</h3>
+                    <p>A Lei 9.096/1995 estabelece regras gerais para criação e registro dos partidos. A Resolução do
+                        TSE nº 23.282/2010, por sua vez, detalha o que é necessário para se criar uma nova legenda. Quem
+                        defere ou não a criação do partido é o TSE. Até a protocolização do pedido neste Tribunal, o
+                        partido em formação deve cumprir exigências, tais como: a elaboração do programa e do estatuto
+                        por pelo menos 101 fundadores; o apoio de eleitores correspondente a, no mínimo, 0,5% dos votos
+                        dados na última eleição geral para a Câmara dos Deputados, distribuído por um terço ou mais dos
+                        Estados; e a certidão de apoio dos eleitores, emitida por cartórios eleitorais.</p>
+                    <h3 id="titulo-3">Cláusula de desempenho</h3>
+                    <p>De acordo com o artigo 41-A da Lei dos Partidos Políticos, 5% do total do Fundo Partidário deve
+                        ser distribuído, em partes iguais, a todas as legendas, e os outros 95%, na proporção dos votos
+                        obtidos pelas agremiações na última eleição geral para a Câmara dos Deputados. Contudo, a Emenda
+                        Constitucional (EC) nº 97/2017 trouxe mudanças ao estabelecer que apenas as siglas que cumprirem
+                        os requisitos de acesso estipulados pela cláusula de desempenho terão direito aos valores do
+                        Fundo.</p>
+                    <p>Atualmente, recebem os recursos do Fundo as agremiações que atingiram no pleito de 2018 pelo
+                        menos 1,5% dos votos válidos nas eleições para a Câmara dos Deputados, distribuídos em pelo
+                        menos um terço das unidades federativas, com no mínimo 1% de votos válidos em cada uma delas.
+                        Ou, ainda, as siglas que elegeram ao menos nove deputados federais distribuídos em pelo menos um
+                        terço das 27 unidades da Federação. Esse cálculo será válido até 2022, já que a EC prevê o
+                        ajuste escalonado do percentual até o ano de 2030.</p>
+                    <p>A cláusula de desempenho também estabelece novos critérios para o tempo de propaganda eleitoral
+                        gratuita no rádio e na televisão. O desempenho eleitoral exigido das legendas partidárias será
+                        aplicado de forma gradual e alcançará seu ápice nas eleições de 2030, conforme previsto na EC nº
+                        97/2017.</p>
+                    <h3 id="titulo-4">Coligações</h3>
+                    <p>A EC nº 97 também vedou, a partir de 2020, a celebração de coligações nas eleições proporcionais
+                        para a Câmara dos Deputados, Câmara Legislativa, assembleias legislativas e câmaras municipais.
+                        Um dos principais reflexos da mudança se dará no ato do pedido de registro de candidaturas à
+                        Justiça Eleitoral, especialmente porque, com o fim das coligações, cada partido deverá,
+                        individualmente, indicar o mínimo de 30% de mulheres filiadas para concorrer no pleito.</p>
+                    <h3 id="titulo-5">Propaganda Partidária e Fundo Especial</h3>
+                    <p>Em 2017, a Lei nº 13.488 extinguiu a propaganda partidária no rádio e na TV. Outra Lei, a de nº
+                        13.487, do mesmo ano, instituiu o Fundo Especial de Financiamento de Campanha (FEFC). Com o fim
+                        da propaganda partidária, os valores de compensação fiscal das emissoras de rádio e televisão
+                        passaram para a constituição do FEFC.</p>
+                    <p>O Fundo concentra recursos que serão utilizados pelos partidos políticos para financiar as
+                        campanhas eleitorais de seus candidatos. Com a proibição de doações por parte de pessoas
+                        jurídicas, estabelecida pelo Supremo Tribunal Federal (STF), o FEFC tornou-se uma das principais
+                        fontes de receita para a realização das campanhas.</p>
+                    <h3 id="titulo-6">Candidatura de mulheres</h3>
+                    <p>De acordo com o secretário judiciário do TSE, Fernando Alencastro, a partir de 2020, as legendas
+                        deverão encaminhar à Justiça Eleitoral, juntamente com o Demonstrativo de Regularidade de Atos
+                        Partidários (DRAP), a lista de candidatas que concorrerão no pleito, respeitando-se o percentual
+                        mínimo de 30% e o máximo de 70% para candidaturas de cada sexo. A regra está prevista no artigo
+                        10, parágrafo 3º da Lei nº 9.504/1997 (Lei das Eleições).</p>
+
+                </div>
+                <div role="tabpanel" class="tab-pane" id="ancora-2"></div>
+                <div role="tabpanel" class="tab-pane" id="ancora-3"></div>
+                <div role="tabpanel" class="tab-pane" id="ancora-4"></div>
+            </div>
+            <?php include __DIR__ . '/inc/noticias/paragrafo-importante.inc.php';?>
+            <?php include __DIR__ . '/inc/noticias/aviso.inc.php';?>
+
+        </section>
+        <?php include __DIR__ . '/inc/noticias/aside-noticias.inc.php';?>
+        <?php include __DIR__ . '/inc/noticias/barra-ferramentas-noticias.inc.php';?>
+
+    </div>
+</div>
+
+<?php include __DIR__ . '/inc/footer.inc.php';?>
