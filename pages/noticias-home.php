a26004cca17f3d7c08660642a49fcc5fe1f390b2
Edivaldo Silva <piqueno43@gmail.com>
2020-12-03 12:11:14 -0300

    Estrutura inicial

---
+++ pages/noticias-home.php
@@ -0,0 +1,26 @@
+<?php
+include('config.inc.php');
+$asideinterna         = false;
+$asidenoticia         = false;
+$titulo               = 'Rádio';
+$compartilhar         = false;
+$tags                 = false;
+$gestor               = false;
+$maisvisitados        = false;
+$paginacao            = false;
+include BASE_URL . '/includes/header.inc.php';
+// Edivaldo
+include BASE_URL . '/includes/noticias/home/topo.inc.php';
+include BASE_URL . '/includes/noticias/home/cards.inc.php';
+// Standby
+include BASE_URL . '/includes/noticias/home/tv.inc.php';
+// Anderson
+include BASE_URL . '/includes/noticias/home/principais-assuntos.inc.php';
+include BASE_URL . '/includes/noticias/home/mais-lidas.inc.php';
+// João/Edivaldo
+include BASE_URL . '/includes/noticias/home/radio.inc.php';
+// João/Edivaldo
+include BASE_URL . '/includes/noticias/home/playlist.inc.php';
+include BASE_URL . '/includes/noticias/home/campanhas.inc.php';
+include BASE_URL . '/includes/footer.inc.php';
+?>
