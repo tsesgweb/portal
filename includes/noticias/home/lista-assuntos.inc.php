e8f42926b3774ab923b5d284ddd7ef4053bf842b
Anderson Granja da Silva dos Santos <andersin100por100to@gmail.com>
2020-12-15 16:37:00 -0300

    Criação seção Principais assuntos

---
+++ includes/noticias/home/lista-assuntos.inc.php
@@ -0,0 +1,60 @@
+<div class="lista-assunto">
+  <ul class="list-unstyled assuntos">
+    <li class="media">
+      <a href="#"><img class="img" src="https://picsum.photos/200/300?random=1" alt="Lorem Ipsum"></a>
+      <div class="media-body">
+        <h3 class="titulo"><a href="#">Lorem Ipsum é simplismente uma simulação de texto da indústria tipográfica
+            e de impressos</a></h3>
+      </div>
+    </li>
+    <li class="media">
+      <a href="#"><img class="img" src="https://picsum.photos/200/200?random=2" alt="Lorem Ipsum"></a>
+      <div class="media-body">
+        <h3 class="titulo"><a href="#">Lorem Ipsum é simplismente uma simulação de texto da indústria tipográfica
+            e de impressos</a></h3>
+      </div>
+    </li>
+    <li class="media">
+      <a href="#"><img class="img" src="https://picsum.photos/200/300?random=3" alt="Lorem Ipsum"></a>
+      <div class="media-body">
+        <h3 class="titulo"><a href="#">Lorem Ipsum é simplismente uma simulação de texto da indústria tipográfica
+            e de impressos</a></h3>
+      </div>
+    </li>
+    <li class="media">
+      <a href="#"><img class="img" src="https://picsum.photos/200/300?random=4" alt="Lorem Ipsum"></a>
+      <div class="media-body">
+        <h3 class="titulo"><a href="#">Lorem Ipsum é simplismente uma simulação de texto da indústria tipográfica
+            e de impressos</a></h3>
+      </div>
+    </li>
+    <li class="media">
+      <a href="#"><img class="img" src="https://picsum.photos/200/300?random=5" alt="Lorem Ipsum"></a>
+      <div class="media-body">
+        <h3 class="titulo"><a href="#">Lorem Ipsum é simplismente uma simulação de texto da indústria tipográfica
+            e de impressos</a></h3>
+      </div>
+    </li>
+    <li class="media">
+      <a href="#"><img class="img" src="https://picsum.photos/200/300?random=6" alt="Lorem Ipsum"></a>
+      <div class="media-body">
+        <h3 class="titulo"><a href="#">Lorem Ipsum é simplismente uma simulação de texto da indústria tipográfica
+            e de impressos</a></h3>
+      </div>
+    </li>
+    <li class="media">
+      <a href="#"><img class="img" src="https://picsum.photos/200/300?random=7" alt="Lorem Ipsum"></a>
+      <div class="media-body">
+        <h3 class="titulo"><a href="#">Lorem Ipsum é simplismente uma simulação de texto da indústria tipográfica
+            e de impressos</a></h3>
+      </div>
+    </li>
+    <li class="media">
+      <a href="#"><img class="img" src="https://picsum.photos/200/300?random=8" alt="Lorem Ipsum"></a>
+      <div class="media-body">
+        <h3 class="titulo"><a href="#">Lorem Ipsum é simplismente uma simulação de texto da indústria tipográfica
+            e de impressos</a></h3>
+      </div>
+    </li>
+  </ul>
+</div>
