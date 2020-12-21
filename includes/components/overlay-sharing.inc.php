eb39188685100ca548c481f804397ce5e1d19360
Edivaldo Silva <piqueno43@gmail.com>
2020-12-21 15:20:50 -0300

    Inclur estilo card radio

--- includes/components/overlay-sharing.inc.php
+++ includes/components/overlay-sharing.inc.php
@@ -1,4 +1,4 @@
-<div class="news-list-sharing-overlay news-list-sharing-overlay_color-news">
+<div class="news-list-sharing-overlay news-list-sharing-overlay_color-<?php $noticia->badge === 'radio'? print('radio'):print('comunicado');?>">
   <button class="news-list-sharing-overlay__close">X</button>          
   <span class="news-list-sharing-overlay__text">matéria</span>
   <span class="news-list-sharing-overlay__title"><?= $noticia->title; ?></span>
