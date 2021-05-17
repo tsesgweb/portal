5fe1d07924f28f7626f51e2eece3c3d632e157bb
Edivaldo Silva <piqueno43@gmail.com>
2021-05-17 12:37:41 -0300

    Gestor responsavel exposicao

---
+++ includes/exposicao/gestor.inc.php
@@ -0,0 +1,10 @@
+<section id="panel-gestor" class="panel-gestor">
+  <div class="panel-gestor__linha">
+    <div class="panel-gestor__conteudo">    
+      <div class="panel-gestor__content">
+        <?php include BASE_URL."/includes/widgets/gestor.inc.php";?>
+      </div>
+    </div>
+  </div>
+</section>
+
