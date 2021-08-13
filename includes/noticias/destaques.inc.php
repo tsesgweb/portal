f3acf9d55f9fbd9509caef4dac33fb12f4b53e92
Edivaldo Silva <piqueno43@gmail.com>
2021-08-13 17:10:00 -0300

    Ajuste noticia sem imagem

--- includes/noticias/destaques.inc.php
+++ includes/noticias/destaques.inc.php
@@ -12,7 +12,7 @@ $noticias = [
   alias.'
 ],
 [
-"img" => 'https://placeimg.com/600/310/any', 
+"img" => '', 
 "tag"=> 'noticia',
  "title"=> "Decisões do Plenário: TSE arquiva ação contra Fernando Haddad por suposto abuso de poder político",
  "description" => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. A fugiat
@@ -27,12 +27,14 @@ $noticias = [
     <h2 class="titulo"><?= $titulo; ?></h2>
     <div class="noticia-listagem_destaque">
       <?php foreach ($noticias as $key => $value) { ?>
-      <article class="noticia-listagem_card">
+      <article class="noticia-listagem_card <?php !$value['img'] ? print('noticia-listagem_card__not-img') : null;?>">
+        <?php if($value['img']): ?>
         <figure class="noticia-listagem_card-img">
         <img src="<?= $value['img'];?>"           
           alt="<?= $value['title'];?>">
         </figure>
-        <div class="noticia-listagem_card-img-overlay">
+        <?php endif; ?>
+        <div class="noticia-listagem_card-<?php $value['img'] ? print('img-overlay') : print('item');?>">
           <span class="noticia-listagem_badge badge-<?= $value['tag']?>"><?= $value['tag'];?></span>
           <h3 class="noticia-listagem_title"><a href="http://"><?= $value['title'];?></a>
           </h3>
