ba9baaa31ca9f86d45c026672475e01e1388a6a5
Edivaldo Silva <piqueno43@gmail.com>
2021-11-11 09:50:25 -0300

    Fix bugs imprensa

--- pages/assets/js/nova-imprensa.js
+++ pages/assets/js/nova-imprensa.js
@@ -211,9 +211,13 @@ $(window).on("load", function () {
       criar_div_conteudo(width, height, div_conteudo, conteudo);
     }, 1000);
 
+  }); 
+ 
+  $(document).on('click', ['.fechar'], function (e) {
+    closeCarousel();
   });
 
-  $(document).on('click', '.fechar', function () {
+  function closeCarousel() {
     $(".owl-nav button").prop("disabled", false);
     $(".owl-stage-outer.owl-height").css({
       height: "auto"
@@ -224,161 +228,7 @@ $(window).on("load", function () {
     setTimeout(function () {
       $(".overlay-conteudo").remove();
     }, 300);
-  });
-
-
-  //    
-  // var $owl_item = $('.carousel-faixa-tv .item');
-
-  // var tamanho_cards_fechado;
-  // var tamanho_cards_aberto;
-
-  // var xs = window.matchMedia("(min-width: 0px) and (max-width: 576px)");
-  // var sm = window.matchMedia("(min-width: 577px) and (max-width: 768px)");
-  // var md = window.matchMedia("(min-width: 769px) and (max-width: 992px)");
-  // var lg = window.matchMedia("(min-width: 993px) and (max-width: 1199px)");
-  // var xl = window.matchMedia("(min-width: 1200px)");
-
-  // if (xs.matches) {
-  //   // console.log("xs");
-  //   var tamanho_cards_fechado = 300;
-  //   var tamanho_cards_aberto = 300;
-  // } else if (sm.matches) {
-  //   // console.log("sm");
-  //   var tamanho_cards_fechado = 310;
-  //   var tamanho_cards_aberto = 500;
-  // } else if (md.matches) {
-  //   // console.log("md");
-  //   var tamanho_cards_fechado = 330;
-  //   var tamanho_cards_aberto = 700;
-  // } else if (lg.matches) {
-  //   // console.log("lg");
-  //   var tamanho_cards_fechado = 350;
-  //   var tamanho_cards_aberto = 900;
-  // } else if (xl.matches) {
-  //   // console.log("xl");
-  //   var tamanho_cards_fechado = 370;
-  //   var tamanho_cards_aberto = tamanho_cards_fechado * 3;
-  // }
-
-  // var tamanho_cards_fechado_px = tamanho_cards_fechado + "px";
-  // var tamanho_cards_aberto_px = tamanho_cards_aberto + "px";
-
-  // $owl_item.each(function () {
-  //   $(this).css("width", tamanho_cards_fechado_px);
-  // });
-
-  // function settings_carousel_faixa() {
-
-  //   $owl.children().each(function (index) {
-  //     $(this).attr('data-position', index);
-  //   });
-
-  //   $('.carousel-faixa-tv').owlCarousel({
-  //     loop: true,
-  //     merge: true,
-  //     center: true,
-  //     responsiveClass: true,
-  //     nav: true,
-  //     margin: 10,
-  //     navText: ["", ""],
-  //     autoWidth: true,
-  //     responsive: {
-  //       0: {
-  //         items: 1,
-  //         autoHeight: true,
-  //         mergeFit: true
-  //       },
-  //       576: {
-  //         items: 2,
-  //         autoHeight: true,
-  //         mergeFit: true
-  //       },
-  //       768: {
-  //         items: 3,
-  //         autoHeight: true,
-  //         mergeFit: true
-  //       },
-  //       992: {
-  //         items: 4,
-  //         autoHeight: true,
-  //         mergeFit: true
-  //       },
-  //       1200: {
-  //         items: 5,
-  //         autoHeight: true,
-  //         mergeFit: false
-  //       }
-  //     }
-  //   });
-  // }
-
-  // function click_abrir_cards(posicao) {
-
-  //   $(".control-cft .posicao").text(posicao);
-  //   var posicao_guardada = $(".control-cft .posicao").text();
-  //   $owl.trigger('destroy.owl.carousel');
-  //   $owl_item.css("width", tamanho_cards_fechado_px);
-  //   $owl_item.removeClass("item-aberto");
-  //   $owl_item.addClass("fechado");
-  //   $owl_item.eq(posicao_guardada).css("width", tamanho_cards_aberto_px);
-  //   $owl_item.eq(posicao_guardada).addClass("item-aberto");
-  //   $owl_item.eq(posicao_guardada).removeClass("fechado");
-  //   $owl_item.eq(posicao_guardada).prependTo('.carousel-faixa-tv');
-
-  //   $owl_item.each(function (index, e) {
-  //     $(this).attr("data-position", index);
-  //   });
-
-  //   $owl.addClass("item-aberto");
-
-  //   settings_carousel_faixa();
-  // }
-
-  // function click_fechar_card() {
-  //   $owl.trigger('destroy.owl.carousel');
-  //   $owl_item.css("width", "350px");
-  //   $owl_item.each(function (index, e) {
-  //     $(this).attr("data-position", index);
-  //   });
-
-  //   settings_carousel_faixa();
-  // }
-
-  // settings_carousel_faixa();
-
-  // $(document).on('click', '.owl-item > div', function (e) {
-
-  //   var posicao_click = $(this).attr("data-position");
-  //   var situacao_item = $(this).hasClass("item-aberto");
-
-  //   $this = $(this);
-
-  //   if (!situacao_item) {
-
-  //     var $speed = 300;
-  //     $owl.trigger('to.owl.carousel', [$this.data('position'), $speed]);
-
-  //     setTimeout(function () {
-  //       click_abrir_cards(posicao_click);
-  //     }, 700);
-
-  //     return false;
-  //   }
-  // });
-
-  // $(".carousel-faixa-tv .item .fechar").on('click', function () {
-
-  //   $(this).closest(".item").removeClass("item-aberto");
-  //   $(this).closest(".item").addClass("fechado");
-
-  //   setTimeout(function () {
-  //     click_fechar_card();
-  //   }, 100);
-
-  //   return false;
-  // });
-
+  }
 
   // Adicionar class na coluna lateral quando não tiver conteúdo na página
   // Ordena os elementos
@@ -443,3 +293,25 @@ $(window).on("load", function () {
     });
   }
 })();
+
+
+(function () {
+  var miniBanners = $('.mini-banners');
+  
+  if(miniBanners.find('.radio-lista').length > 4) {
+
+    miniBanners.addClass('owl-carousel owl-theme');
+    
+    miniBanners.find('.radio-lista').css({'min-width': '100%'});
+    
+    miniBanners.owlCarousel({
+      items: 4,    
+      margin: 15,         
+      dots: true,
+      autoplay: true,
+      loop: false,
+      nav: true,
+    });
+  }
+
+})(jQuery);
--- pages/noticias-radio-interna-sem-conteudo.php
+++ pages/noticias-radio-interna-sem-conteudo.php
@@ -12,22 +12,118 @@ $relacionadas   = false;
 $internanoticia = true;
 include BASE_URL . '/includes/header.inc.php';
 ?>
-<div class="noticia__conteudo">
-    <div class="noticia__linha">
-        <section id="texto-conteudo" class="texto-noticias-internas">
-            <div class="tab-content sem-galeria">
-                <div role="tabpanel" class="tab-pane active" id="ancora-1"></div>
-                <div role="tabpanel" class="tab-pane" id="ancora-2"></div>
-                <div role="tabpanel" class="tab-pane" id="ancora-3"></div>
-                <div role="tabpanel" class="tab-pane" id="ancora-4"></div>
-            </div>
-            <?php include BASE_URL . '/includes/noticias/paragrafo-importante.inc.php';?>
-            <?php include BASE_URL . '/includes/noticias/aviso.inc.php';?>
+<div class="noticia__conteudo template-audioradio">
+  <div class="noticia__linha">
+    <section id="texto-conteudo" class="texto-noticias-internas">
+      <div class="tab-content sem-galeria">
+        <div role="tabpanel" class="tab-pane active" id="ancora-1"></div>
+        <div role="tabpanel" class="tab-pane" id="ancora-2"></div>
+        <div role="tabpanel" class="tab-pane" id="ancora-3"></div>
+        <div role="tabpanel" class="tab-pane" id="ancora-4"></div>
+      </div>
+      <?php include BASE_URL . '/includes/noticias/paragrafo-importante.inc.php';?>
+      <?php include BASE_URL . '/includes/noticias/aviso.inc.php';?>
 
-        </section> 
-        <?php include BASE_URL . '/includes/widgets/aside-noticias-radio.inc.php';?>
-        <?php include BASE_URL . '/includes/noticias/barra-ferramentas-noticias.inc.php';?>
-    </div>
+    </section>
+    <aside id="coluna-lateral-noticias" class="lateral nao-imprimir ancora-sem-conteudo">
+      <h2 class="sr-only">Coluna relacionada a rádio</h2>
+      <div class="portletWrapper"
+        data-portlethash="706c6f6e652e7269676874636f6c756d6e0a636f6e746578740a2f706f7274616c6a652f7473652f696d7072656e73612f726164696f0a756c74696d61732d64612d726164696f"
+        id="portletwrapper-706c6f6e652e7269676874636f6c756d6e0a636f6e746578740a2f706f7274616c6a652f7473652f696d7072656e73612f726164696f0a756c74696d61732d64612d726164696f">
+        <div class="lateral__ultimas">
+          <header class="lateral__ultimas-header">
+            <h4 class="lateral__ultimas-title">
+              Últimas da rádio
+            </h4>
+          </header>
+          <article class="lateral__ultimas-items">
+            <figure class="ultimas-imagem"><img src="https://picsum.photos/274/147"
+                alt="Sessão plenária do TSE em 16.10.2014"></figure>
+            <h5 class="ultimas-titulo"><a
+                href="https://portalhomologa.tse.jus.br/tse/imprensa/radio/2021/Novembro/audio-de-teste-da-radio"
+                title="Link: Áudio de teste da rádio">
+                Áudio de teste da rádio
+              </a></h5>
+          </article>
+          <article class="lateral__ultimas-items">
+            <figure class="ultimas-imagem"><img src="https://picsum.photos/274/147" alt="Titulo expo abertura"></figure>
+            <h5 class="ultimas-titulo"><a
+                href="https://portalhomologa.tse.jus.br/tse/imprensa/radio/2021/Agosto/lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-sed-do-eiusmod-tempor-incididunt-ut-labore-et-dolore-magna-aliqua"
+                title="Link: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.
+              </a></h5>
+          </article>
+          <article class="lateral__ultimas-items">
+            <figure class="ultimas-imagem"><img src="https://picsum.photos/274/147">
+            </figure>
+            <h5 class="ultimas-titulo"><a
+                href="https://portalhomologa.tse.jus.br/tse/imprensa/radio/2021/Junho/aberta-consulta-publica-sobre-aplicacao-das-resolucoes-do-tse-nas-eleicoes-2020-04-06-2021-12-48-00"
+                title="Link: Aberta consulta pública sobre aplicação das resoluções do TSE nas Eleições 2020">
+                Aberta consulta pública sobre aplicação das resoluções do TSE nas Eleições 2020
+              </a></h5>
+          </article>
+          <article class="lateral__ultimas-items">
+            <figure class="ultimas-imagem"><img src="https://picsum.photos/274/147">
+            </figure>
+            <h5 class="ultimas-titulo"><a
+                href="https://portalhomologa.tse.jus.br/tse/imprensa/radio/2021/Junho/aberta-consulta-publica-sobre-aplicacao-das-resolucoes-do-tse-nas-eleicoes-2020-04-06-2021-12-48-00"
+                title="Link: Aberta consulta pública sobre aplicação das resoluções do TSE nas Eleições 2020">
+                Aberta consulta pública sobre aplicação das resoluções do TSE nas Eleições 2020
+              </a></h5>
+          </article>
+        </div>
+      </div>
+      <div class="portletWrapper"
+        data-portlethash="706c6f6e652e7269676874636f6c756d6e0a636f6e746578740a2f706f7274616c6a652f7473652f696d7072656e73612f726164696f0a70726f6772616d6173"
+        id="portletwrapper-706c6f6e652e7269676874636f6c756d6e0a636f6e746578740a2f706f7274616c6a652f7473652f696d7072656e73612f726164696f0a70726f6772616d6173">
+        <div class="portletWrapper">
+          <header class="mini-banners__header">
+            <h4 class="mini-banners__title">
+              Playlist
+            </h4>
+          </header>
+          <div class="mini-banners">
+            <div class="radio-lista radio-lista-01">
+              <div class="radio-lista-body"><a class="btn btn-link stretched-link"
+                  href="https://portalhomologa.tse.jus.br/tse/imprensa/radio?program=diario-das-eleicoes">
+                  Diário das Eleições
+                </a></div>
+            </div>
+            <div class="radio-lista radio-lista-02">
+              <div class="radio-lista-body"><a class="btn btn-link stretched-link"
+                  href="https://portalhomologa.tse.jus.br/tse/imprensa/radio?program=conexao-eleitoral">
+                  Conexão eleitoral
+                </a></div>
+            </div>
+            <div class="radio-lista radio-lista-03">
+              <div class="radio-lista-body"><a class="btn btn-link stretched-link"
+                  href="https://portalhomologa.tse.jus.br/tse/imprensa/radio?program=por-dentro-da-justica-eleitoral">
+                  Por dentro da Justiça Eleitoral
+                </a></div>
+            </div>
+            <div class="radio-lista radio-lista-04">
+              <div class="radio-lista-body"><a class="btn btn-link stretched-link"
+                  href="https://portalhomologa.tse.jus.br/tse/imprensa/radio?program=sessao-plenaria">
+                  Sessão Plenária
+                </a></div>
+            </div>
+            <div class="radio-lista radio-lista-05">
+              <div class="radio-lista-body"><a class="btn btn-link stretched-link"
+                  href="https://portalhomologa.tse.jus.br/tse/imprensa/radio?program=teste-de-programa">
+                  Teste de programa
+                </a></div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </div>
+    </aside>
+    <?php include BASE_URL . '/includes/noticias/barra-ferramentas-noticias.inc.php';?>
+  </div>
 </div>
 
 <?php include BASE_URL . '/includes/footer.inc.php';?>
