c27f1af10db4313aa7686a1f6a3fd5fc94a8882d
Edivaldo Silva <piqueno43@gmail.com>
2022-02-08 14:03:07 -0300

    Incluir kwai redes sociais

--- includes/home/redes-sociais.inc.php
+++ includes/home/redes-sociais.inc.php
@@ -1,57 +1,58 @@
 <!-- Inicio Redes Sociais-->
 <section class="redes-sociais__conteudo">
-    <h2 class="titulo-redes-sociais">Acompanhe nossas redes</h2>
-    <div class="veiculos-redes mt-5 mb-5">
-        <div class="redes__linha">
-            <h3 class="rede-card">
-                <a href="#">
-                    <span class="card-imagem"><img src="assets/imagens/redes-sociais/facebook.png" alt="ícone do Facebook"></span>
-                    <span class="card-titulo">Facebook</span>
-                </a>
-            </h3>
-            <h3 class="rede-card">
-                <a href="#">
-                    <span class="card-imagem"><img src="assets/imagens/redes-sociais/twitter.png" alt="ícone do Twitter"></span>
-                    <span class="card-titulo">Twitter</span>
-                </a>
-            </h3>
-            <h3 class="rede-card">
-                <a href="#">
-                    <span class="card-imagem"><img src="assets/imagens/redes-sociais/instagram.png" alt="ícone do Instagram"></span>
-                    <span class="card-titulo">Instagram</span>
-                </a>
-            </h3>
-            <h3 class="rede-card">
-                <a href="#">
-                    <span class="card-imagem"><img src="assets/imagens/redes-sociais/youtube.png" alt="ícone do Youtube"></span>
-                    <span class="card-titulo">YouTube</span>
-                </a>
-            </h3>
-            <h3 class="rede-card">
-                <a href="#">
-                    <span class="card-imagem"><img src="assets/imagens/redes-sociais/flikr.png" alt="ícone do Flickr"></span>
-                    <span class="card-titulo">Flickr</span>
-                </a>
-            </h3>
-            <h3 class="rede-card">
-                <a href="#">
-                    <span class="card-imagem"><img src="assets/imagens/redes-sociais/soundcloud.png" alt="ícone do SoundCloud"></span>
-                    <span class="card-titulo">SoundCloud</span>
-                </a>
-            </h3>
-            <h3 class="rede-card">
-                <a href="https://www.tiktok.com/@tsejus">
-                    <span class="card-imagem"><img src="assets/imagens/redes-sociais/tiktok.png" alt="ícone do TikTok"></span>
-                    <span class="card-titulo">TikTok</span>
-                </a>
-            </h3>
-            <h3 class="rede-card">
-                <a href="https://br.linkedin.com/company/tsejus">
-                    <span class="card-imagem"><img src="assets/imagens/redes-sociais/linkedin.png" alt="ícone do LinkedIn"></span>
-                    <span class="card-titulo">LinkedIn</span>
-                </a>
-            </h3>
-        </div>
+  <h2 class="titulo-redes-sociais">Acompanhe nossas redes</h2>
+  <div class="veiculos-redes mt-5 mb-5">
+    <div class="redes__linha">
+      <h3 class="rede-card">
+        <a href="#">
+          <span class="card-imagem"><img src="assets/imagens/redes-sociais/facebook.png" alt="ícone do Facebook"></span>
+          <span class="card-titulo">Facebook</span>
+        </a>
+      </h3>
+      <h3 class="rede-card">
+        <a href="#">
+          <span class="card-imagem"><img src="assets/imagens/redes-sociais/twitter.png" alt="ícone do Twitter"></span>
+          <span class="card-titulo">Twitter</span>
+        </a>
+      </h3>
+      <h3 class="rede-card">
+        <a href="#">
+          <span class="card-imagem"><img src="assets/imagens/redes-sociais/instagram.png"
+              alt="ícone do Instagram"></span>
+          <span class="card-titulo">Instagram</span>
+        </a>
+      </h3>
+      <h3 class="rede-card">
+        <a href="#">
+          <span class="card-imagem"><img src="assets/imagens/redes-sociais/youtube.png" alt="ícone do Youtube"></span>
+          <span class="card-titulo">YouTube</span>
+        </a>
+      </h3>
+      <h3 class="rede-card">
+        <a href="#">
+          <span class="card-imagem"><img src="assets/imagens/redes-sociais/flikr.png" alt="ícone do Flickr"></span>
+          <span class="card-titulo">Flickr</span>
+        </a>
+      </h3>
+      <h3 class="rede-card">
+        <a href="https://www.tiktok.com/@tsejus">
+          <span class="card-imagem"><img src="assets/imagens/redes-sociais/tiktok.png" alt="ícone do TikTok"></span>
+          <span class="card-titulo">TikTok</span>
+        </a>
+      </h3>
+      <h3 class="rede-card">
+        <a href="https://br.linkedin.com/company/tsejus">
+          <span class="card-imagem"><img src="assets/imagens/redes-sociais/linkedin.png" alt="ícone do LinkedIn"></span>
+          <span class="card-titulo">LinkedIn</span>
+        </a>
+      </h3>
+      <h3 class="rede-card">
+        <a href="https://br.linkedin.com/company/tsejus">
+          <span class="card-imagem"><img src="assets/imagens/redes-sociais/kwai.png" alt="ícone do Kwai"></span>
+          <span class="card-titulo">Kwai</span>
+        </a>
+      </h3>
     </div>
+  </div>
 </section>
 <!-- Fim Redes Sociais-->
