9d4f76f4b756206f68391c6c43b54af23e10f712
Edivaldo Silva <edivaldo.silva43@gmail.com>
2022-03-31 15:06:42 -0300

    Bandeira de idiomas

--- includes/topo-acessibilidade.inc.php
+++ includes/topo-acessibilidade.inc.php
@@ -1,64 +1,86 @@
 <!-- start: topo-acessibilidade -->
 <div class="acessibilidade acessibilidade_bg-azul">
-    <div class="acessibilidade__conteudo">
-        <div class="acessibilidade__linha">
-            <div class="atalhos">
-                <div class="acessibilidade_inline-block">
-                    <a href="#destaque-principal" name="destaque-principal" accesskey="1" class="acessibilidade__link acessibilidade_oculto">
-                        <span>Conteúdo principal [1]</span>
-                    </a>
-                </div>
-                <div class="acessibilidade_inline-block">
-                    <a href="#navegacaoResposiva" accesskey="2" class="acessibilidade__link acessibilidade_oculto">
-                        <span>Menu principal [2]</span>
-                    </a>
-                </div>
-                <div class="acessibilidade_inline-block">
-                    <a href="#atalho-busca" accesskey="3" class="acessibilidade__link acessibilidade_oculto">
-                        <span>Busca [3]</span>
-                    </a>
-                </div>
-                <div class="acessibilidade_inline-block">
-                    <a href="http://www.tse.jus.br/busca_avancada" accesskey="5" class="acessibilidade__link acessibilidade_oculto">
-                        <span>Busca avançada [5]</span>
-                    </a>
-                </div>
-                <div class="acessibilidade_inline-block">
-                    <a href="http://www.tse.jus.br/" accesskey="6" class="acessibilidade__link acessibilidade_oculto">
-                        <span>Página inicial [6]</span>
-                    </a>
-                </div>
-                <div class="acessibilidade_inline-block">
-                    <a href="http://www.tse.jus.br/acessibilidades-tse" accesskey="7" class="acessibilidade__link acessibilidade_oculto">
-                        <span>Página de acessibilidade [7]</span>
-                    </a>
-                </div>
-            </div>
-            <div class="transparencia">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a id="transparencia_link-acessibilidade" class=" transparencia_link" title="Acessibilidade" href="http://www.tse.jus.br/acessibilidades-tse" target="_self">
-                            Acessibilidade                            
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a id="transparencia_link-faleconosco" class=" transparencia_link" title="Fale conosco" href="http://www.tse.jus.br/eleitor/servicos/ouvidoria" target="_self">
-                            Fale conosco                           
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="transparencia_link" title="Transparência" href="http://www.tse.jus.br/transparencia" target="_self">
-                            Transparência
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="http://www.tse.jus.br/transparencia/acesso-a-informacao/acesso-a-informacao" class="transparencia_link" data-toggle="tooltip" data-placement="botton" title="" target="_self" data-original-title="Acesso à informação">
-                            <img src="assets/imagens/footer/acesso-informacao.png" alt="Imagem acesso à Informação">
-                        </a>
-                    </li>
-                </ul>
-            </div>
+  <div class="acessibilidade__conteudo">
+    <div class="acessibilidade__linha">
+      <div class="atalhos">
+        <div class="acessibilidade_inline-block">
+          <a href="#destaque-principal" name="destaque-principal" accesskey="1"
+            class="acessibilidade__link acessibilidade_oculto">
+            <span>Conteúdo principal [1]</span>
+          </a>
         </div>
+        <div class="acessibilidade_inline-block">
+          <a href="#navegacaoResposiva" accesskey="2" class="acessibilidade__link acessibilidade_oculto">
+            <span>Menu principal [2]</span>
+          </a>
+        </div>
+        <div class="acessibilidade_inline-block">
+          <a href="#atalho-busca" accesskey="3" class="acessibilidade__link acessibilidade_oculto">
+            <span>Busca [3]</span>
+          </a>
+        </div>
+        <div class="acessibilidade_inline-block">
+          <a href="http://www.tse.jus.br/busca_avancada" accesskey="5"
+            class="acessibilidade__link acessibilidade_oculto">
+            <span>Busca avançada [5]</span>
+          </a>
+        </div>
+        <div class="acessibilidade_inline-block">
+          <a href="http://www.tse.jus.br/" accesskey="6" class="acessibilidade__link acessibilidade_oculto">
+            <span>Página inicial [6]</span>
+          </a>
+        </div>
+        <div class="acessibilidade_inline-block">
+          <a href="http://www.tse.jus.br/acessibilidades-tse" accesskey="7"
+            class="acessibilidade__link acessibilidade_oculto">
+            <span>Página de acessibilidade [7]</span>
+          </a>
+        </div>
+      </div>
+      <div class="transparencia">
+        <ul class="nav">
+          <li class="nav-item">
+            <a id="transparencia_link-acessibilidade" class=" transparencia_link" title="Acessibilidade"
+              href="http://www.tse.jus.br/acessibilidades-tse" target="_self">
+              Acessibilidade
+            </a>
+          </li>
+          <li class="nav-item">
+            <a id="transparencia_link-faleconosco" class=" transparencia_link" title="Fale conosco"
+              href="http://www.tse.jus.br/eleitor/servicos/ouvidoria" target="_self">
+              Fale conosco
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="transparencia_link" title="Transparência" href="http://www.tse.jus.br/transparencia"
+              target="_self">
+              Transparência e prestações de contas
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="http://www.tse.jus.br/transparencia/acesso-a-informacao/acesso-a-informacao"
+              class="transparencia_link" data-toggle="tooltip" data-placement="botton" title="" target="_self"
+              data-original-title="Acesso à informação">
+              <img src="assets/imagens/footer/acesso-informacao.png" alt="Imagem acesso à Informação">
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="idioma">
+        <ul class="nav">
+          <li class="nav-item">
+            <a href="#ingles">
+              <img src="assets/imagens/topo/ingles.png" alt="ingles" />
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#espanhol">
+              <img src="assets/imagens/topo/espanhol.png" alt="espanhol" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </div>
 <!-- end: topo-acessibilidade -->
