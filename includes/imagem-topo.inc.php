35c616d181232b41cd3b344ae41b2c608f0b47db
Anderson Granja <andersin100por100to@gmail.com>
2022-07-08 11:43:05 -0300

    Ajuste na legenda e transcrição da imagem principal páginas internas

--- includes/imagem-topo.inc.php
+++ includes/imagem-topo.inc.php
@@ -1,7 +1,8 @@
 <figure id="imagem-principal-texto" class="text-center">
 <a data-fancybox="galeria" class="video" data-caption="Seja Mesário Voluntário - A Justiça Eleitoral prioriza a sua segurança
 " href="https://www.youtube.com/watch?v=NyzoGw1CKFs">
-        <img src="http://www.justicaeleitoral.jus.br/imagens/imagens/livros/image" alt="# Vem Pra Urna em">
+        <img src="https://www.tse.jus.br/administracao/destaques/tse-celebra-acordo-com-a-oea-para-receber-missao-de-observacao-nas-eleicoes-2022/@@images/f9089557-0770-4085-b3a1-2d8de46e32df.jpeg" alt="# Vem Pra Urna em">
     </a>
-    <figcaption class="descricao-img-principal">Acesse a transcrição textual do vídeo</figcaption>
+    <figcaption class="descricao-img-principal">Descrição da imagem</figcaption>
+    <div class="transcricao-img-principal"><a href="#" class="link">Acesse a transcrição textual do vídeo</a></div>
 </figure>
